24b3d30ceb37b7e95c902f6e92e48dd884921e94
voice of texnoforge <voice@texnoforge.dev>
2021-04-27 04:44:15 +0200

    woplang: new CLI to test TexnoMagicLanguage

---
+++ wopeditor/woplang.py
@@ -0,0 +1,29 @@
+import argparse
+import json
+import sys
+
+
+from wopeditor.texnomagic import lang
+
+
+def woplang(*cargs):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('word', nargs='+', help='Words of Power')
+    args = parser.parse_args(cargs)
+    txlang = lang.TexnoMagicLanguage()
+    text = " ".join(args.word)
+    out = txlang.parse(text)
+    try:
+        print(json.dumps(out, indent=4, sort_keys=False))
+    except TypeError:
+        print(out)
+    return 0
+
+
+def main():
+    cargs = sys.argv[1:]
+    sys.exit(woplang(*cargs))
+
+
+if __name__ == '__main__':
+    main()
